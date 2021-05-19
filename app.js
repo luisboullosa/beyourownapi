f84cbced9ef3a88d2d08fdab19e27f5912fe6272
Luis Boullosa <luis.boullosa@evobio.eu>
2021-05-19 12:00:40 +0200

    Created routes and organized

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express')
-
+const express = require('express');
+const Mongoose = require('mongoose');
 const app = express();
+require('dotenv/config');
+// Import routes
+const postsRoute= require('./routes/posts')
+//Middleware
+/* app.use('/posts', () => {
+    console.log('This is a middleware running')
+}) */
 
-
-// ROUTES
+//ROUTES
+// Passed on to routes folder
+app.use('/posts', postsRoute)
 app.get('/', (req,res) => {
     res.send('We are on home');
 })
+
+//Connect to DB;
+Mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true , useUnifiedTopology: true},() => 
+console.log('Connected to DB!')
+);
 
 // LISTENER
 app.listen(3000);
