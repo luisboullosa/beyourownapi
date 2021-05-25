637c5a05bb2a94074152b02870c4e5d58a7870f4
Luis Boullosa <luis.boullosa@evobio.eu>
2021-05-25 14:26:44 +0200

    All working

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
+var ObjectId = require('mongodb').ObjectId; 
 
+//GETS ALL THE POSTS
+router.get('/', async (req,res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message:err})
+    }
+});
 
-/* router.get('/', (req,res) => {
-    res.send('We are on posts');
-}); */
-
+//SUBMITS POST
 router.post('/', async (req,res) => {
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    });
-
     try {
+        const post = new Post({
+            title: req.body.title,
+            description: req.body.description
+        });
         const savedPost = await post.save();
         res.json(savedPost);
-    } catch(err){
-        res.json({message: err})
+    } catch(Err){
+        res.json({message: Err})
     }
-    
 });
+
+//GET SPECIFIC POST
+router.get('/:postId', async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({message: err});
+        console.error(err);
+    }
+});
+
+router.delete('/:postId', async (req,res) => {
+    try{
+        const removedPost = await Post.deleteOne({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({message: err});
+    }
+});
+
+router.patch('/:postId', async (req, res) => {
+    try{
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId }, 
+            { $set: {title: req.body.title} });
+        res.json(updatedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+})
 
 module.exports = router;
